5055a2ffa471c1d00072b4b3bd37f6076970b66d
ColleSerre <61471511+ColleSerre@users.noreply.github.com>
2020-08-23 00:18:30 +0200

    added dropdown buttons ; submit button now returns JSON data of selected checkboxes from TOSDR API

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-$.getJSON("https://tosdr.org/api/1/service/facebook.json").done(
-                (data) => document.getElementById("##").innerHTML = JSON.stringify(data)
-                );
-            function alertCheckbox() {
-                var elements = document.getElementById("services").elements;
-                for (var i = 0; i < elements.length; i++){
-                    element = elements[i];
-                    if (element.checked == true) {
-                        console.log(`${element.value} is checked`)
-                    } else {
-                        console.log(`${element.value} is not checked`)
-                    }
-                }
-            }
+function alertCheckbox() {
+  var elements = document.getElementById("services").elements;
+  for (var i = 0; i < elements.length; i++) {
+    element = elements[i];
+    if (element.checked == true) {
+      console.log(element.value);
+      var url = `https://tosdr.org/api/1/service/${element.value}.json`;
+      console.log(url);
+      $.getJSON(url).done((data) => console.log(JSON.stringify(data)));
+    }
+  }
+}
